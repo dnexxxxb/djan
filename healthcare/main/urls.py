ab2a4b01db55ff903307bd1ce9c2eafc8a01e52b
dnexxxxb <wcharllote60@gmail.com>
2026-07-01 14:19:50 +0100

    first commit

--- healthcare/main/urls.py
+++ healthcare/main/urls.py
@@ -1,6 +1,16 @@
-from django.contrib import admin
 from django.urls import path
+from . import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.home, name='home'),
+    path("about/", views.about, name='about'),
+    path("contact/", views.contact, name='contact'),
+    path("doctors/", views.doctors, name='doctors'),
+    path("pravacy/", views.privacy, name='privacy'),
+    path("terms/", views.terms, name='terms'),
+    path("services", views.services, name='services'),
+    path("testimonials/", views.testimonials, name='testimonials'),
+    path("depertment/", views.depertment, name='depertment'),
+    path("appointment/", views.appointment, name='appointment'),
+
 ]
--- healthcare/main/views.py
+++ healthcare/main/views.py
@@ -4,6 +4,30 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html')
 
-def single(request):
-    return render(request, 'single.html')
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def doctors(request):
+    return render(request, 'doctors.html')
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+def terms(request):
+    return render(request, 'terms.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def testimonials(request):
+    return render(request, 'testimonials.html')
+
+def depertment(request):
+    return render(request, 'depertment.html')
+
+def appointment(request):
+    return render(request, 'appointment.html')
 
